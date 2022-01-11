793ffb2285c8c9c536ab8f634216608c189b243c
Rafał Szopa <rafal.szopa@op.pl>
2022-01-11 16:56:55 +0100

    dodanie ruchów w grze

--- js/script.js
+++ js/script.js
@@ -28,8 +28,29 @@ if(playerInput == '1'){
 else if(playerInput == '2'){
     playerMove = 'papier';
 }
-else(playerInput == '3'){
+else if(playerInput == '3'){
     playerMove = 'nożyce';
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  }
+  else if(computerMove == 'papier' && playerMove == 'nożyce'){
+      printMessage('Ty wygrywasz');
+  }
+  else if (computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz');
+  }
+  else if (computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis');
+  }
+ else if (computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Remis');
+  }
+ else if (computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis');
+  }
+  else if(computerMove == 'nożyce' && playerMove == 'nieznany ruch'){
+    printMessage('Nieznany ruch');
