376b75aaeeb4fa032504d7457d22e078fe5242a3
Rafał Szopa <rafal.szopa@op.pl>
2022-01-11 22:56:19 +0100

    dodanie ruchów gracza

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
+let computerMove = getMoveName(randomNumber);
+let randomNumber = getMoveName ('1 : kamień, 2 : papier, 3 : nożyce')
 
+/*
 if(randomNumber == 1){
   computerMove = 'kamień';
 }
@@ -12,16 +14,17 @@ else if(randomNumber ==2){
 }
 else if(randomNumber ==3){
     computerMove = 'nożyce';
-}
-
+} 
+*/
 printMessage('Mój ruch to: ' + computerMove);
 
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce');
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
+let playerMove = getMoveName(playerInput);
 
+/*
 if(playerInput == '1'){
   playerMove = 'kamień';
 }
@@ -31,9 +34,32 @@ else if(playerInput == '2'){
 else if(playerInput == '3'){
     playerMove = 'nożyce';
 }
-
+*/
 printMessage('Twój ruch to: ' + playerMove);
 
+displayResult(argComputerMove, argPlayerMove){
+ if(argComputerMove == 2 && argPlayerMove ==1) {
+    return 'Wygrywasz'
+    }
+else if(argComputerMove == 1 && argPlayerMove ==3) {
+    return 'Wygrywasz'
+}
+else if(argComputerMove == 3 && argPlayerMove ==2) {
+    return 'Wygrywasz'
+}
+else if(argComputerMove == 1 && argPlayerMove ==1) {
+    return 'Remis'
+}
+else if(argComputerMove == 2 && argPlayerMove ==2) {
+        return 'Remis'  
+}
+else if(argComputerMove == 3 && argPlayerMove ==3) {
+            return 'Remis' 
+}
+printMessage ('Nie znam ruchu o id' + argPlayerMove + '.');
+return 'nieznany ruch'
+
+/*
 if( computerMove == 'kamień' && playerMove == 'papier'){
     printMessage('Ty wygrywasz!');
   }
@@ -54,3 +80,4 @@ if( computerMove == 'kamień' && playerMove == 'papier'){
   }
   else if(computerMove == 'nożyce' && playerMove == 'nieznany ruch'){
     printMessage('Nieznany ruch');
+    */
